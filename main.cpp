ca9e4f4fd6f151e441dc35578ba3b43d9f7bd017
mariya29k <maria.kachaunova@gmail.com>
2021-01-21 20:25:16 +0200

    added tests

--- main.cpp
+++ main.cpp
@@ -5,15 +5,27 @@
 int main()
 {
     DLList<int> first;
-    first.pushback(1);
-    first.pushback(2);
+    first.pushback(7);
     first.pushback(3);
-    first.pushback(4);
+    first.pushback(2);
+    first.pushback(8);
     first.pushback(5);
-    first.pushback(6);
+    first.pushback(1);
+    first.pushback(5);
+    first.pushback(2);
+    first.pushback(5);
+    first.pushback(1);
+
 
     first.print();
     std::cout<<"*****************";
+    first.removeOccurences();
+    first.print();
+    std::cout<<"\n*****************\n";
+    //first.mergeSort(first);
+    std::cout<< first.getLast() << "\n";
+    std::cout << first.getFirst();
+    
 
     return 0;
 }
